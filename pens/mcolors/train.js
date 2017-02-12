a121304e3b4d0ec7801b177ce901092b331cdf85
Airead Fan <fgh1987168@gmail.com>
2017-02-12 23:10:38 +0800

    use deltaE to compare color

--- pens/mcolors/train.js
+++ pens/mcolors/train.js
@@ -4,7 +4,7 @@ var gameState = {
         into answer: keep last step rgb, and show the anwser of color
     */
     state: 'answer', // ['color', 'answer'], 
-    rgb: '#eeeeee',
+    rgb: colorDefs['green']['100'],
     cname: 'white'
 };
 
@@ -112,7 +112,9 @@ function getDistanceBetweenRgb(rgb1, rgb2) {
     var o1 = splitRgb(rgb1);
     var o2 = splitRgb(rgb2);
 
-    return Math.pow((o1.r - o2.r), 2) + Math.pow((o1.g - o2.g), 2) + Math.pow((o1.b - o2.b), 2);
+    // return Math.pow((o1.r - o2.r), 2) + Math.pow((o1.g - o2.g), 2) + Math.pow((o1.b - o2.b), 2);
+
+    return deltaE(rgb2lab([o1.r, o1.g, o1.b]), rgb2lab([o2.r, o2.g, o2.b]));
 }
 
 function splitRgb(rgb) {
@@ -225,4 +227,46 @@ function showAnswer() {
 
 function hideAnswer() {
     answerContent.innerHTML = '';
+}
+
+function rgb2lab(rgb) {
+    var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255,
+        x, y, z;
+
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+    x = (x > 0.008856) ? Math.pow(x, 1 / 3) : (7.787 * x) + 16 / 116;
+    y = (y > 0.008856) ? Math.pow(y, 1 / 3) : (7.787 * y) + 16 / 116;
+    z = (z > 0.008856) ? Math.pow(z, 1 / 3) : (7.787 * z) + 16 / 116;
+
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
+
+// calculate the perceptual distance between colors in CIELAB
+// https://github.com/THEjoezack/ColorMine/blob/master/ColorMine/ColorSpaces/Comparisons/Cie94Comparison.cs
+
+function deltaE(labA, labB) {
+    var deltaL = labA[0] - labB[0];
+    var deltaA = labA[1] - labB[1];
+    var deltaB = labA[2] - labB[2];
+    var c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    var c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    var deltaC = c1 - c2;
+    var deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    var sc = 1.0 + 0.045 * c1;
+    var sh = 1.0 + 0.015 * c1;
+    var deltaLKlsl = deltaL / (1.0);
+    var deltaCkcsc = deltaC / (sc);
+    var deltaHkhsh = deltaH / (sh);
+    var i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
 }
