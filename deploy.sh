1a8228ff423d4dada67dba80983666a21a334bba
Airead Fan <fgh1987168@gmail.com>
2018-06-19 18:16:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -avz -e "ssh -p 2222" pens airead@123.56.101.172:www
-rsync -avz -e "ssh -p 2222" lib airead@123.56.101.172:www
+rsync -avz -e "ssh -p 22" pens airead@www.i64.top:www
+rsync -avz -e "ssh -p 22" lib airead@www.i64.top:www
